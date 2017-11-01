62f85a9b43e796a5fc283565a2b08c66c2e96171
BBungBa <sook00773@gmail.com>
2017-11-01 19:20:47 +0900

    Solve all

--- Q11/Q11/1.c
+++ Q11/Q11/1.c
@@ -9,13 +9,13 @@
 //
 //	if ((fp = fopen("data.dat", "r")) == NULL)
 //	{
-//		printf("File Open Error!!!");
+//		printf("file open error!!!");
 //		exit(-1);
 //	}
-//	fseek(fp, 3, SEEK_SET); // TXT 파일이 UTF - 8 이라면  BOM을 없애기 위해 3byte 땅김
+//	fseek(fp, 3, SEEK_SET); // txt 파일이 utf - 8 이라면  bom을 없애기 위해 3byte 땅김
 //
 //	int count = 0, line = 1, i = 0;
-//	while ((fgets(str, 100, fp)) != NULL) // EOF 가면 NULL ptr 반환
+//	while ((fgets(str, 100, fp)) != NULL) // eof 가면 null ptr 반환
 //	{
 //		for (i = 0; str[i] != '\n'; i++) if (str[i] == 'a') count++;
 //		printf("#%d : %d\n", line++, count);
--- Q11/Q11/10.c
+++ Q11/Q11/10.c
@@ -1,34 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h> // 저수준 io
-#include <sys\stat.h>  // pmode parm
-#include <sys\types.h> // <sys\stat.h>에 선언되어 있지만 에러가 날 수 있으니 선언해줌
-
-#define BUFFERSIZE 4096
-
-int main()
-{
-	int infd, outfd;
-	char buffer[BUFFERSIZE];
-
-	if ((infd = open("swallowing.mp4", O_RDONLY | O_BINARY)) < 0)
-	{
-		perror("Can't open file sleepy.png\n");
-		exit(-1);
-	}
-
-	if ((outfd = open("copy.mp4", O_CREAT | O_WRONLY | O_BINARY)) < 0)
-	{
-		perror("Can't open file copy.png\n");
-		exit(-1);
-	}
-
-	while (read(infd, buffer, BUFFERSIZE))
-	{
-		write(outfd, buffer, BUFFERSIZE);
-	}
-	close(infd);
-	close(outfd);
-
-	return 0;
-}
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <fcntl.h> // 저수준 io
+//#include <sys\stat.h>  // pmode parm
+//#include <sys\types.h> // <sys\stat.h>에 선언되어 있지만 에러가 날 수 있으니 선언해줌
+//
+//#define BUFFERSIZE 4096
+//
+//int main()
+//{
+//	int infd, outfd;
+//	char buffer[BUFFERSIZE];
+//
+//	if ((infd = open("swallowing.mp4", O_RDONLY | O_BINARY)) < 0)
+//	{
+//		perror("Can't open file sleepy.png\n");
+//		exit(-1);
+//	}
+//
+//	if ((outfd = open("copy.mp4", O_CREAT | O_WRONLY | O_BINARY)) < 0)
+//	{
+//		perror("Can't open file copy.png\n");
+//		exit(-1);
+//	}
+//
+//	while (read(infd, buffer, BUFFERSIZE))
+//	{
+//		write(outfd, buffer, BUFFERSIZE);
+//	}
+//	close(infd);
+//	close(outfd);
+//
+//	return 0;
+//}
